f51c7e7b01b47a42d58862e7fad9e74f2762aafe
Max Wang <mwang@1080agile.com>
2018-06-15 16:49:37 +1000

    init

---
+++ ServiceBusMessageProducer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusMessageProducer
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://edmorder.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=qdClUmM1r1mUtPJKkAOPeY/1O1OhChLJSPEs2QjhWr8=";
+        const string QueueName = "edm.order.zohocrm.job";
+        static IQueueClient queueClient;
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            await queueClient.SendAsync(new Message(Encoding.UTF8.GetBytes("5005")));
+
+            Console.ReadKey();
+
+
+
+            await queueClient.CloseAsync();
+        }
+    }
+}
